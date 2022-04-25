20eeeb29f008ec9c0d8cb9f384c2c6e1cf962f08
bernie-zhu <bwz2@illinois.edu>
2022-04-25 06:07:00 -0500

    added interactivity

--- frontend/src/views/Map/Map.js
+++ frontend/src/views/Map/Map.js
@@ -1,25 +1,82 @@
-import React from "react";
-import { GoogleMap, withScriptjs, withGoogleMap, Marker } from "react-google-maps";
-import * as busStopData from "../../data/busstops.json";
+import React, { useEffect, useState } from "react";
+import { GoogleMap, withScriptjs, withGoogleMap, Marker, InfoWindow } from "react-google-maps";
+import * as busStopData from "../../data/data.json";
+import { Button } from 'antd';
 
-var json = JSON.parse(busStopData);
+import { getFavoriteStops, insertFavoriteStop } from '../../api.js';
+
+var data = JSON.parse(JSON.stringify(busStopData));
+const arr = [];
+for (var i in data) {
+  arr.push(data[i]);
+}
+arr.splice(arr.length - 1, 1);
+arr.splice(arr.length - 1, 1);
 
 function Map() {
+  const [selectedStop, setSelectedStop] = useState(null);
+  var [username, setUsername] = useState(null);
+  var [favoriteStops, setfavoriteStops] = useState([]);
+
+  useEffect(() => {
+      var username = localStorage.getItem('username');
+      if (username) {
+          setUsername(username);
+          getFavoriteStops(username).then(res => {
+              setfavoriteStops(res);
+          });
+      }
+  }, [])
+
+  const onFavorite = (values) => {
+      console.log(values);
+      insertFavoriteStop(username, values).then(res => {
+          getFavoriteStops(username).then(res => {
+              setfavoriteStops(res);
+          });
+      });
+  };
+
   return (
+
     <GoogleMap
-      defaultZoom={10}
+      defaultZoom={15}
       defaultCenter={{ lat:40.1020, lng:-88.2272 }}
     >
-      {json.map((busStop) => (
+      {arr.map((busStop) => (
         <Marker
           key = {busStop.stop_id}
           position = {{
-            lat:busStop.stop_points[0].stop_lat,
-            lng:busStop.stop_points[0].stop_lon
+            //lat: 40.114512,
+            //lng: -88.180673
+            lat: busStop.stop_points[0].stop_lat,
+            lng: busStop.stop_points[0].stop_lon
+          }}
+          onClick={() => {
+            setSelectedStop(busStop);
           }}
         />
       ))}
-    </GoogleMap>
+
+      {selectedStop && (
+        <InfoWindow
+          position = {{
+            //lat: 40.114512,
+            //lng: -88.180673
+            lat: selectedStop.stop_points[0].stop_lat,
+            lng: selectedStop.stop_points[0].stop_lon
+          }}
+          onCloseClick={() => {
+            setSelectedStop(null);
+          }}
+        >
+          <div>
+            <h2>{selectedStop.stop_name} - {selectedStop.stop_id}</h2>
+            <Button onClick={() => onFavorite(selectedStop.stop_id)}>Favorite</Button>
+          </div>
+        </InfoWindow>
+      )}
+      </GoogleMap>
   );
 }
 
